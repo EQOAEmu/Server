5a82ce611ca5f5f6e139534959e7ff71de702d22
KrazusEQOA <krazuseqoa@yahoo.com>
2015-06-06 00:32:56 -0400

    working on 2nd packet

--- opcodes.h
+++
@@ -1,5 +0,0 @@
-#define E_CODE  224               // [E0]
-#define SESSION_REQUEST 33        // [E0 21]
-#define SESSION_CLOSE   20        // [E0 14]
-#define SERVER_COMPLEX_REQUEST 1  // [E0 01]
-#define CHARACTER_REQUEST 112     // [70]
